d5478c903d49bf8a85ec9485565965de56f9eba4
Adeniji Olajide <adenijiolajid01.com>
2023-07-16 03:56:00 +0100

    Implement serial code generator functionality with cooperative background tasks

---
+++ backgroundtasks/README.md
@@ -0,0 +1,53 @@
+# Serial Code Generator
+
+The **Serial Code Generator** is a web application that demonstrates the use of cooperatively scheduled background tasks using the `requestIdleCallback()` method. It allows users to generate a specified number of serial codes and displays them in a log container.
+
+With this Serial Code Generator, you can easily generate a large number of unique codes for various purposes, such as voucher codes, activation keys, or promotional offers. The code generation process is optimized to run efficiently in the background, ensuring smooth performance even for generating a large quantity of codes.
+
+Try out the live example [here](https://example.com) to see the Cooperative Serial Code Generator in action!
+
+Please note that if you choose to generate a large number of serial codes, it may take some time to complete the processing. The application is designed to handle such scenarios while keeping the user interface interactive and responsive.
+
+## Key Features
+
+- Input field for specifying the desired number of serial codes to generate.
+- Progress bar to indicate the current progress of the code generation process.
+- Log container to display the generated serial codes in a scrollable manner.
+- Start button to initiate the code generation process.
+- End button to stop the code generation process.
+- Responsive and user-friendly interface.
+
+## Getting Started
+
+To use the Serial Code Generator, simply open the web application in a modern web browser. Enter the desired number of serial codes in the input field and click the "Start" button. The application will generate the codes in the background, and you can monitor the progress through the progress bar and log container. If needed, you can stop the code generation process by clicking the "End" button.
+
+> **Note**: Please ensure that you enter a valid number greater than zero in the input field. The button will be disabled during the generation process to prevent multiple simultaneous requests.
+
+## Technologies Used
+
+The Serial Code Generator is built with the following technologies:
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Installation
+
+To install the Serial Code Generator locally, follow these steps:
+
+1. Clone the repository:
+
+    ```shell
+    git clone https://github.com/your-username/serial-code-generator.git
+    ```
+2. Open the project directory:
+    ```shell
+    cd serial-code-generator
+    ```
+3. Open the index.html file in your preferred web browser.
+
+## Contributing
+Contributions to the Serial Code Generator are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+### Contact
+For any inquiries or feedback, please contact adenijiolajid01@gmail.com.
---
+++ backgroundtasks/main.js
@@ -0,0 +1,145 @@
+// Get references to HTML elements
+let lengthText = document.getElementById('length');
+let start = document.getElementById('startButton');
+let end = document.getElementById('endButton');
+let progressText = document.getElementById('progressText');
+let logElem = document.getElementById('serial');
+let logContainer = document.getElementById('log');
+let progressBarElem = document.querySelector('progress');
+
+// Variables to manage the code generation process
+let taskHandler = null;
+let chunktaskHandler = null;
+let Taskend = false;
+
+// Available characters for generating serial codes
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+// Array to store generated serial codes
+let serialCode = [];
+
+// Variables to track the progress and display the generated codes
+let currentTaskNumber = 0;
+let serial = null;
+let logFragment = null;
+let codeChunkLength = 0;
+
+// Function to append a log message to the log container
+function log(text) {
+    if (!logFragment) {
+        logFragment = document.createDocumentFragment();
+    }
+
+    const elem = document.createElement('span');
+    elem.textContent = text;
+    logFragment.appendChild(elem);
+}
+
+// Function to update the progress bar, log, and progress text
+function updateDisplay() {
+    progressBarElem.max = lengthText.value;
+    progressBarElem.value = currentTaskNumber + 1;
+    log(serial);
+    logElem.appendChild(logFragment);
+    progressText.innerText = 'Generated ' + (currentTaskNumber + 1) + ' of ' + lengthText.value;
+
+    // Scroll log container to the bottom if necessary
+    const scrolledToEnd = logContainer.scrollHeight - logContainer.clientHeight <= logContainer.scrollTop + 1;
+    if (!scrolledToEnd) {
+        logContainer.scrollTop = logContainer.scrollHeight - logContainer.clientHeight;
+    }
+}
+
+// Function to validate the input number
+function validateNumber(value) {
+    if (value === '' || isNaN(value) || parseInt(value) <= 0 || parseInt(value) > Number.MAX_SAFE_INTEGER) {
+        return false;
+    }
+    return true;
+}
+
+// Function to generate background tasks for serial code generation
+function generateBackgroundTasks(deadline) {
+    while ((deadline.timeRemaining() > 0 || deadline.didTimeout) && serialCode.length && !Taskend) {
+        serial = serialCode.shift();
+        updateDisplay();
+        currentTaskNumber++;
+    }
+
+    if (serialCode.length && !Taskend) {
+        taskHandler = requestIdleCallback(generateBackgroundTasks, { timeout: 1000 });
+    } else {
+        lengthText.disabled = false;
+        start.disabled = false;
+        taskHandler = null;
+    }
+}
+
+// Function to generate a chunk of serial codes
+function generateCodeChunk(deadline) {
+    while ((deadline.timeRemaining() > 0 || deadline.didTimeout) && codeChunkLength < lengthText.value && !Taskend) {
+        let code = '';
+        for (let j = 0; j < 8; j++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            code += characters.charAt(randomIndex);
+        }
+        serialCode.push(code);
+        codeChunkLength++;
+
+        // If the desired number of codes is reached, start generating background tasks
+        if (codeChunkLength >= lengthText.value) {
+            logElem.innerText = null;
+            taskHandler = requestIdleCallback(generateBackgroundTasks, { timeout: 1000 });
+            break;
+        }
+    }
+
+    // Continue generating code chunks if more codes are needed
+    if (codeChunkLength < lengthText.value && !Taskend) {
+        chunktaskHandler = requestIdleCallback(generateCodeChunk, { timeout: 1000 });
+    } else {
+        chunktaskHandler = null;
+        taskHandler = requestIdleCallback(generateBackgroundTasks, { timeout: 1000 });
+    }
+}
+
+// Function to initiate the serial code generation process
+function generateSerialCode() {
+    const value = lengthText.value.trim();
+
+    if (!validateNumber(value)) {
+        alert('Please enter a valid number greater than zero.');
+        return;
+    }
+
+    logElem.innerText = 'Processing Data....';
+    currentTaskNumber = 0;
+    codeChunkLength = 0;
+    lengthText.disabled = true;
+    start.disabled = true;
+    Taskend = false;
+
+    chunktaskHandler = requestIdleCallback(generateCodeChunk, { timeout: 1000 });
+}
+
+// Function to stop the serial code generation process
+function stopProcess() {
+    if (logElem.innerText === 'Processing Data....') {
+        logElem.innerText = null;
+    }
+    if (chunktaskHandler) {
+        cancelIdleCallback(chunktaskHandler);
+        chunktaskHandler = null;
+    }
+    if (taskHandler) {
+        cancelIdleCallback(taskHandler);
+        taskHandler = null;
+    }
+    lengthText.disabled = false;
+    start.disabled = false;
+    Taskend = true;
+}
+
+// Event listeners for the "Start" and "End" buttons
+start.addEventListener('click', generateSerialCode);
+end.addEventListener('click', stopProcess);
